b071ea53c5b57251331fc57a2cf8c022178b8551
Eduardo Almeida <eduardo.oliveira@facens.br>
2021-02-23 19:29:04 -0300

    feat(jpa): added initial configurations

--- src/main/java/com/facens/poo/sistema/sistema/controllers/ClientController.java
+++ src/main/java/com/facens/poo/sistema/sistema/controllers/ClientController.java
@@ -1,10 +1,9 @@
 package com.facens.poo.sistema.sistema.controllers;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import com.facens.poo.sistema.sistema.dto.ClientDTO;
-import com.facens.poo.sistema.sistema.entities.Client;
 import com.facens.poo.sistema.sistema.services.ClientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
