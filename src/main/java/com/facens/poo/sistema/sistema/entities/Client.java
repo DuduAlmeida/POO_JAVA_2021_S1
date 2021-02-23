9962017b927ae096378e2b1f282ad9338e79b1bc
Eduardo Almeida <eduardo.oliveira@facens.br>
2021-02-23 20:08:23 -0300

    created a jpa application

--- src/main/java/com/facens/poo/sistema/sistema/entities/Client.java
+++ src/main/java/com/facens/poo/sistema/sistema/entities/Client.java
@@ -1,9 +1,32 @@
 package com.facens.poo.sistema.sistema.entities;
 
-public class Client {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_CLIENT")
+public class Client implements Serializable {
+
+    /* #region Private Properties */
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String address;
+
+    /* #endregion Private Properties */
+
+    /* #region Public Methods */
 
     public Long getId() {
         return id;
@@ -11,6 +34,10 @@ public class Client {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public String getName() {
@@ -21,14 +48,46 @@ public class Client {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /* #endregion Public Methods */
 
-    
+    /* #region JPA Methods */
+
+    /*** 
+     * Método que retorna o hash code do objeto
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    /*** 
+     * Método que compara dois objetos dessa classe e verificar se são iguais, 
+     * nesse caso somente o id é o suficiente para isso
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    /* #endregion JPA Methods */
+
 }
